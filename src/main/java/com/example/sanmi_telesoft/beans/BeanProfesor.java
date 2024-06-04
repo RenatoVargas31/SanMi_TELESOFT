8368f59a42e62a66140d3c88def12cdb19a9fd9c
Sergio <a20202137@pucp.edu.pe>
2024-06-04 01:08:14 -0500

    Bean Serenazgo Profesores

--- src/main/java/com/example/sanmi_telesoft/beans/BeanProfesor.java
+++ src/main/java/com/example/sanmi_telesoft/beans/BeanProfesor.java
@@ -1,4 +1,50 @@
 package com.example.sanmi_telesoft.beans;
 
 public class BeanProfesor {
+
+    private int idProfesores;
+    private String nombreProfesor;
+    private String apellidoProfesor;
+    private String dniProfesor;
+    private String cursoProfesor;
+
+    public int getIdProfesores() {
+        return idProfesores;
+    }
+
+    public void setIdProfesores(int idProfesores) {
+        this.idProfesores = idProfesores;
+    }
+
+    public String getNombreProfesor() {
+        return nombreProfesor;
+    }
+
+    public void setNombreProfesor(String nombreProfesor) {
+        this.nombreProfesor = nombreProfesor;
+    }
+
+    public String getApellidoProfesor() {
+        return apellidoProfesor;
+    }
+
+    public void setApellidoProfesor(String apellidoProfesor) {
+        this.apellidoProfesor = apellidoProfesor;
+    }
+
+    public String getDniProfesor() {
+        return dniProfesor;
+    }
+
+    public void setDniProfesor(String dniProfesor) {
+        this.dniProfesor = dniProfesor;
+    }
+
+    public String getCursoProfesor() {
+        return cursoProfesor;
+    }
+
+    public void setCursoProfesor(String cursoProfesor) {
+        this.cursoProfesor = cursoProfesor;
+    }
 }
--- src/main/java/com/example/sanmi_telesoft/beans/BeanSerenazgo.java
+++ src/main/java/com/example/sanmi_telesoft/beans/BeanSerenazgo.java
@@ -1,4 +1,76 @@
 package com.example.sanmi_telesoft.beans;
 
 public class BeanSerenazgo {
+    private int idSerenazgos;
+    private String nombreSereno;
+    private String apellidoSereno;
+    private String dniSereno;
+    private String turnoSereno;
+    private String direccionSereno;
+    private String telefonoSereno;
+    private int idTipoSereno;
+
+    public int getIdSerenazgos() {
+        return idSerenazgos;
+    }
+
+    public void setIdSerenazgos(int idSerenazgos) {
+        this.idSerenazgos = idSerenazgos;
+    }
+
+    public String getNombreSereno() {
+        return nombreSereno;
+    }
+
+    public void setNombreSereno(String nombreSereno) {
+        this.nombreSereno = nombreSereno;
+    }
+
+    public String getApellidoSereno() {
+        return apellidoSereno;
+    }
+
+    public void setApellidoSereno(String apellidoSereno) {
+        this.apellidoSereno = apellidoSereno;
+    }
+
+    public String getDniSereno() {
+        return dniSereno;
+    }
+
+    public void setDniSereno(String dniSereno) {
+        this.dniSereno = dniSereno;
+    }
+
+    public String getTurnoSereno() {
+        return turnoSereno;
+    }
+
+    public void setTurnoSereno(String turnoSereno) {
+        this.turnoSereno = turnoSereno;
+    }
+
+    public String getDireccionSereno() {
+        return direccionSereno;
+    }
+
+    public void setDireccionSereno(String direccionSereno) {
+        this.direccionSereno = direccionSereno;
+    }
+
+    public String getTelefonoSereno() {
+        return telefonoSereno;
+    }
+
+    public void setTelefonoSereno(String telefonoSereno) {
+        this.telefonoSereno = telefonoSereno;
+    }
+
+    public int getIdTipoSereno() {
+        return idTipoSereno;
+    }
+
+    public void setIdTipoSereno(int idTipoSereno) {
+        this.idTipoSereno = idTipoSereno;
+    }
 }
