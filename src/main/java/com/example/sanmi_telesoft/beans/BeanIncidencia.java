ce49917f7a72c2fc2c9e54857ba6c66f3dabe646
Jaimito <151692936+JaimitoSky@users.noreply.github.com>
2024-06-14 11:53:49 -0500

    Update BeanIncidencia.java

--- src/main/java/com/example/sanmi_telesoft/beans/BeanIncidencia.java
+++ src/main/java/com/example/sanmi_telesoft/beans/BeanIncidencia.java
@@ -1,4 +1,123 @@
 package com.example.sanmi_telesoft.beans;
 
 public class BeanIncidencia {
+    private int idIncidencias;
+    private String nombreIncidencia;
+    private String lugarIncidencia;
+    private String referenciaIncidencia;
+    private String fotoIncidencia;
+    private String victimaIncidencia;
+    private boolean requiereAmbulancia;
+    private boolean requierePolicia;
+    private String descripcionSolucion;
+    private int idUsuario;
+    private int idTipoIncidencia;
+    private String nombreUsuario;
+    private String nombreUrbanizacion;
+
+    // Getters y Setters
+    public int getIdIncidencias() {
+        return idIncidencias;
+    }
+
+    public void setIdIncidencias(int idIncidencias) {
+        this.idIncidencias = idIncidencias;
+    }
+
+    public String getNombreIncidencia() {
+        return nombreIncidencia;
+    }
+
+    public void setNombreIncidencia(String nombreIncidencia) {
+        this.nombreIncidencia = nombreIncidencia;
+    }
+
+    public String getLugarIncidencia() {
+        return lugarIncidencia;
+    }
+
+    public void setLugarIncidencia(String lugarIncidencia) {
+        this.lugarIncidencia = lugarIncidencia;
+    }
+
+    public String getReferenciaIncidencia() {
+        return referenciaIncidencia;
+    }
+
+    public void setReferenciaIncidencia(String referenciaIncidencia) {
+        this.referenciaIncidencia = referenciaIncidencia;
+    }
+
+    public String getFotoIncidencia() {
+        return fotoIncidencia;
+    }
+
+    public void setFotoIncidencia(String fotoIncidencia) {
+        this.fotoIncidencia = fotoIncidencia;
+    }
+
+    public String getVictimaIncidencia() {
+        return victimaIncidencia;
+    }
+
+    public void setVictimaIncidencia(String victimaIncidencia) {
+        this.victimaIncidencia = victimaIncidencia;
+    }
+
+    public boolean isRequiereAmbulancia() {
+        return requiereAmbulancia;
+    }
+
+    public void setRequiereAmbulancia(boolean requiereAmbulancia) {
+        this.requiereAmbulancia = requiereAmbulancia;
+    }
+
+    public boolean isRequierePolicia() {
+        return requierePolicia;
+    }
+
+    public void setRequierePolicia(boolean requierePolicia) {
+        this.requierePolicia = requierePolicia;
+    }
+
+    public String getDescripcionSolucion() {
+        return descripcionSolucion;
+    }
+
+    public void setDescripcionSolucion(String descripcionSolucion) {
+        this.descripcionSolucion = descripcionSolucion;
+    }
+
+    public int getIdUsuario() {
+        return idUsuario;
+    }
+
+    public void setIdUsuario(int idUsuario) {
+        this.idUsuario = idUsuario;
+    }
+
+    public int getIdTipoIncidencia() {
+        return idTipoIncidencia;
+    }
+
+    public void setIdTipoIncidencia(int idTipoIncidencia) {
+        this.idTipoIncidencia = idTipoIncidencia;
+    }
+
+    public String getNombreUsuario() {
+        return nombreUsuario;
+    }
+
+    public void setNombreUsuario(String nombreUsuario) {
+        this.nombreUsuario = nombreUsuario;
+    }
+
+    public String getNombreUrbanizacion() {
+        return nombreUrbanizacion;
+    }
+
+    public void setNombreUrbanizacion(String nombreUrbanizacion) {
+        this.nombreUrbanizacion = nombreUrbanizacion;
+    }
 }
+
