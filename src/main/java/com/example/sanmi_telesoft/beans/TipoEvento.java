7fd294cf1d46512a9b6605dfc1de674210c2aded
AlejandroHancco <142966841+AlejandroHancco@users.noreply.github.com>
2024-06-15 00:31:46 -0500

    Add files via upload

---
+++ src/main/java/com/example/sanmi_telesoft/beans/TipoEvento.java
@@ -0,0 +1,22 @@
+package com.example.sanmi_telesoft.beans;
+
+public class TipoEvento {
+    private int idTipoEvento;
+    private String nameTipo;
+
+    public int getIdTipoEvento() {
+        return idTipoEvento;
+    }
+
+    public void setIdTipoEvento(int idEstadoEvento) {
+        this.idTipoEvento = idEstadoEvento;
+    }
+
+    public String getNameTipo() {
+        return nameTipo;
+    }
+
+    public void setNameTipo(String nameEstado) {
+        this.nameTipo = nameEstado;
+    }
+}
